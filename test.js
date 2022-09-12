23a299771ba9d206c63fe76c1acd3080d249a4fe
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-09-13 00:29:12 +0600

    error also include

--- test.js
+++ test.js
@@ -2,14 +2,18 @@ let inputField = document.getElementById('inputField');
 let input = inputField.value;
 
 const addTask = () => {
-  console.log(inputField.value)
-  let taskList = document.getElementById('task-list');
-  const divClass = document.createElement('ul');
-  divClass.innerHTML = `
-  <li> ${inputField.value}<img src="close.png" height="8px" width="8px"></li>
-  `
-  taskList.appendChild(divClass);
-  inputField.value = ``;
+
+  if (inputField.value === '') {
+    console.log('khali dekacce sir');
+  }
+  else {
+    let taskList = document.getElementById('task-list');
+    const divClass = document.createElement('ul');
+    divClass.innerHTML = `
+    <li> ${inputField.value}<img src="close.png" height="8px" width="8px"></li>`
+    taskList.appendChild(divClass);
+    inputField.value = ``;
+  }
 }
 const clearAllTask = () => {
   let taskList = document.getElementById('task-list');
