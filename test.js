2c375a96d6ca71ec8460e6c97b85ee849ca259f0
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-09-12 23:56:40 +0600

    UI is ready for coding

---
+++ test.js
@@ -0,0 +1,16 @@
+// let arr = [];
+// arr.forEach(element => {
+//   console.log(element);
+// });
+
+// let arrNew = arr.map(p => p);
+// console.log(arrNew);
+// const name = 'Hero'; const age = 34;
+// const person = { name, age }; console.log(person);
+// const adventurer = { name: 'Alice', cat: { name: 'Lucy' } };
+// const ratName = adventurer?.rat?.name;
+// console.log(ratName);
+// let person = null;
+// console.log(person ? person : "person is null");
+const obj = { a: 1 };
+console.log(Object.keys(obj).length === 0);
